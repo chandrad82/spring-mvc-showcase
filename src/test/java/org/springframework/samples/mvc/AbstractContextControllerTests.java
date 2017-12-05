f19fd38434e25abfa2fea4ec984f80d0e53f1cf3
chandrad82 <33873290+chandrad82@users.noreply.github.com>
2017-12-05 11:43:48 +0530

    Update AbstractContextControllerTests.java

--- src/test/java/org/springframework/samples/mvc/AbstractContextControllerTests.java
+++ src/test/java/org/springframework/samples/mvc/AbstractContextControllerTests.java
@@ -1,3 +1,4 @@
+
 package org.springframework.samples.mvc;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.web.context.WebApplicationContext;
 @WebAppConfiguration
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml")
 public class AbstractContextControllerTests {
-
+	
 	@Autowired
 	protected WebApplicationContext wac;
 
